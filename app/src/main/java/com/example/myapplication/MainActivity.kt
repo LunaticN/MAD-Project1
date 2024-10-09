9f428a8671eb29113fda0bfa8b84fa33ae9747a3
BT_1N3_07 <esgfsdf@sdsd.com>
2024-10-09 09:33:21 -0400

    finished front end

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        val startButton: Button = findViewById(R.id.start)
+        startButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?){
+        TODO( )
     }
 }
