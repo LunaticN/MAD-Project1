d783cb48ce73e8002564155316067b93e372633c
BT_1N3_07 <nayaantaki@gmail.com>
2024-10-11 09:29:09 -0400

    took notes on game algorithm, set up values for buttons through id.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,30 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var gameStarted: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val startButton: Button = findViewById(R.id.start)
-        startButton.setOnClickListener(this)
+        val blueButton: TextView = findViewById(R.id.blue_button)
+        val orangeButton: TextView = findViewById(R.id.orange_button)
     }
 
-    override fun onClick(v: View?){
-        TODO( )
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
+
+    //implement various lambda expressions for each button/clickable thing.
+    //for the orange and blue text views, check to see that the start button has been clicked by updating some kind of instance variable.
 }
